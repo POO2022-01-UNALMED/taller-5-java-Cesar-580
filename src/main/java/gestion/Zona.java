98164c4fe77570b953f57e41f77ae40163a7bb33
Cesar-580 <65823540+Cesar-580@users.noreply.github.com>
2022-04-25 23:52:19 -0500

    .

---
+++ src/main/java/gestion/Zona.java
@@ -0,0 +1,10 @@
+package test;
+import java.util.ArrayList;
+
+public class Zona {
+	private String nombre;
+	private Array<Zona> zoo = new ArrayList<Zona>();
+	private ArrayList<Animal> animales = new ArrayList<Animal>();
+	
+	
+}
---
+++ src/main/java/gestion/Zoologico.java
@@ -0,0 +1,12 @@
+package test;
+import java.util.ArrayList;
+
+public class Zoologico {
+	
+	private String nombre;
+	private String ubicacion;
+	private Array<Zona> zoo = new ArrayList<Zona>();
+	
+	
+	
+}

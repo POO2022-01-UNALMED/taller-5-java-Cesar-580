9ec97b2063a567f7e1ad1b9e70ee4dbf077db287
Cesar-580 <65823540+Cesar-580@users.noreply.github.com>
2022-04-27 22:32:11 -0500

    FIN

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -57,10 +57,14 @@ public class Animal {
 	
 
 	public String toString(){
+		
 		if(this.zona==null) {
+			
 			return ("Mi nombre es "+this.getNombre()+", tengo una edad de "+this.getEdad()+", habito en "+this.getHabitat()+" y mi genero es "+this.getGenero());
 		}
+		
 		else {
+			
 			return ("Mi nombre es "+this.getNombre()+" tengo una edad de "+this.getEdad()+" habito en "+this.getHabitat()+" y mi genero es "+this.getGenero()+", la zona en la que me ubico es "+this.zona);
 		}
 	}
